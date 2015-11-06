44b7e0077ecd48329884716e9d23e0192eaf1a56
Damien L-G <qdi@ornl.gov>
2015-11-06 13:33:27 -0500

    add conversion teuchos array and dealii tensor to helpers

--- src/DTK_DealIIHelpers.h
+++ src/DTK_DealIIHelpers.h
@@ -4,8 +4,10 @@
 #include <no_comment/DTK_DealIIEntityExtraData.h>
 #include <DTK_Entity.hpp>
 #include <Teuchos_RCP.hpp>
+#include <Teuchos_ArrayView.hpp>
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/tria_accessor.h>
+#include <deal.II/base/point.h>
 
 namespace DataTransferKit {
 
@@ -20,6 +22,30 @@ struct DealIIHelpers
         return dealii::TriaIterator<dealii::CellAccessor<dim,spacedim>>(
             Teuchos::rcp_dynamic_cast<DealIIEntityExtraData<dim,dim,spacedim> const>(
                 entity.extraData() )->dealii_tria_accessor );
+    }
+
+    template <int dim>
+    static
+    dealii::Point<dim>
+    getPoint(Teuchos::ArrayView<double const> const & arr)
+    {
+        if (arr.size() != static_cast<size_t>(dim))
+            std::runtime_error("that's not good");
+        dealii::Point<dim> p;
+        for (int d = 0; d < dim; ++d)
+            p[d] = arr[d];
+        return p;
+    }
+
+    template <int spacedim>
+    static
+    Teuchos::Array<double>
+    getArray(dealii::Tensor<1,spacedim> const & t)
+    {
+        Teuchos::Array<double> arr(spacedim);
+        for (int d = 0; d < spacedim; ++d)
+            arr[d] = t[d];
+        return arr;
     }
 
 };
